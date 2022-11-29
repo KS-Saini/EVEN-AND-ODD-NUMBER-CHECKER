bd8586d5890fd4b59c5c71488a220bbba1329dc8
KuldeeSaini <109736186+KuldeeSaini@users.noreply.github.com>
2022-11-29 20:14:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+var number = prompt("Enter the number");
+//console.log(number);
+var result = number / 2;
+var camparison = Math.round(number / 2);
+if (result == camparison) {
+ alert("even")
+} else {
+alert('odd')
+}
